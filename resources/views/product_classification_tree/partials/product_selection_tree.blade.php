7c9088b5994ac33bb3b8da334c51660ddf7f1a71
Mohamed <mo.esmael2019@gmail.com>
2023-02-13 14:59:03 +0200

    f13-2

--- resources/views/product_classification_tree/partials/product_selection_tree.blade.php
+++ resources/views/product_classification_tree/partials/product_selection_tree.blade.php
@@ -83,7 +83,7 @@ $product_selected = !empty($pct_data['product_selected']) ? $pct_data['product_s
                                         </div>
                                     </div>
                                     <div id="collapse{{@replace_space($class->name)}}" class="accordion-body collapse">
-                                        @if(session('system_mode') != 'restaurant')
+
                                         <div class="accordion-inner">
                                             @php
                                             $i = 0;
@@ -215,74 +215,7 @@ $product_selected = !empty($pct_data['product_selected']) ? $pct_data['product_s
                                             @endforeach
 
                                         </div>
-                                        @else
-                                        <div class="accordion-inner">
-                                            @php
-                                            $query =
-                                            App\Models\Product::where('product_class_id',
-                                            $class->id);
 
-                                            $products = $query->select('products.id',
-                                            'products.name', 'products.sku',
-                                            'products.sell_price')->groupBy('products.id')->get();
-                                            @endphp
-                                            @foreach ($products as
-                                            $product)
-
-                                            <div class="row product_row">
-                                                <div class="col-md-1">
-                                                    <input id="product_selected{{$product->id}}"
-                                                        name="pct[product_selected][]" type="checkbox"
-                                                        value="{{$product->id}}" @if(in_array($product->id,
-                                                    $product_selected)) checked @endif
-                                                    class="my-new-checkbox product_checkbox">
-                                                </div>
-                                                <div class="col-md-5">
-                                                    <img src="@if(!empty($product->getFirstMediaUrl('product'))){{$product->getFirstMediaUrl('product')}}@else{{asset('/uploads/'.session('logo'))}}@endif"
-                                                        alt="photo" width="50" height="50">
-                                                    <a href="">{{$product->name}}</a>
-                                                </div>
-                                                @php
-                                                $expiry_date =
-                                                App\Models\AddStockLine::where('product_id',
-                                                $product->id)->whereDate('expiry_date', '>=',
-                                                date('Y-m-d'))->select('expiry_date')->orderBy('expiry_date',
-                                                'asc')->first();
-                                                $current_stock =
-                                                App\Models\ProductStore::where('product_id',
-                                                $product->id)->select(DB::raw('SUM(product_stores.qty_available)
-                                                as current_stock'))->first();
-                                                @endphp
-                                                <div class="col-md-6">
-                                                    <div class="row">
-                                                        <div class="col-md-6">
-                                                            <div class="col-md-12">
-                                                                <label style="color: #222;">@lang('lang.sku'):
-                                                                    {{$product->sku}}</label>
-                                                            </div>
-                                                            <div class="col-md-12">
-                                                                <label style="color: #222;">@lang('lang.expiry'):
-                                                                    @if(!empty($expiry_date)){{@format_date($expiry_date->expiry_date)}}@else{{'N/A'}}@endif</label>
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-md-6">
-                                                            <div class="col-md-12">
-                                                                <label style="color: #222;">@lang('lang.stock'):
-                                                                    @if(!empty($current_stock)){{@num_format($current_stock->current_stock)}}@endif</label>
-                                                            </div>
-                                                            <div class="col-md-12">
-                                                                <label style="color: #222;">@lang('lang.price'):
-                                                                    {{@num_format($product->sell_price)}}</label>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            @endforeach
-                                        </div>
-
-                                        @endif
                                     </div>
                                 </div>
                             </div>
@@ -290,63 +223,7 @@ $product_selected = !empty($pct_data['product_selected']) ? $pct_data['product_s
                         </div>
 
 
-                        {{-- @php
-                            $products_no_class = App\Models\Product::whereNull('product_class_id')->get();
-
-                        @endphp
-                        @foreach ($products_no_class as $product)
-
-
-                        <div class="row product_row">
-                            <div class="col-md-1">
-                                <input id="product_selected{{$product->id}}"
-                                    name="pct[product_selected][]" type="checkbox"
-                                    value="{{$product->id}}" @if(in_array($product->id,
-                                $product_selected)) checked @endif
-                                class="my-new-checkbox product_checkbox">
-                            </div>
-                            <div class="col-md-5">
-                                <img src="@if(!empty($product->getFirstMediaUrl('product'))){{$product->getFirstMediaUrl('product')}}@else{{asset('/uploads/'.session('logo'))}}@endif"
-                                    alt="photo" width="50" height="50">
-                                <a href="">{{$product->name}}</a>
-                            </div>
-                            @php
-                            $expiry_date =
-                            App\Models\AddStockLine::where('product_id',
-                            $product->id)->whereDate('expiry_date', '>=',
-                            date('Y-m-d'))->select('expiry_date')->orderBy('expiry_date',
-                            'asc')->first();
-                            $current_stock =
-                            App\Models\ProductStore::where('product_id',
-                            $product->id)->select(DB::raw('SUM(product_stores.qty_available)
-                            as current_stock'))->first();
-                            @endphp
-                            <div class="col-md-6">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="col-md-12">
-                                            <label style="color: #222;">@lang('lang.sku'):
-                                                {{$product->sku}}</label>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <label style="color: #222;">@lang('lang.expiry'):
-                                                @if(!empty($expiry_date)){{@format_date($expiry_date->expiry_date)}}@else{{'N/A'}}@endif</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="col-md-12">
-                                            <label style="color: #222;">@lang('lang.stock'):
-                                                @if(!empty($current_stock)){{@num_format($current_stock->current_stock)}}@endif</label>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <label style="color: #222;">@lang('lang.price'):
-                                                {{@num_format($product->sell_price)}}</label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach --}}
+                        
 
 
                     </div>
