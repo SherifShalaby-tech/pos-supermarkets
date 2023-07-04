8bcc1b129945c132104d1407fc33dbd574e28221
Raneem-ayman <73843554+Raneem-ayman@users.noreply.github.com>
2023-07-04 13:26:37 +0300

    Update ProductInAdjustmentsController.php

--- app/Http/Controllers/ProductInAdjustmentsController.php
+++ app/Http/Controllers/ProductInAdjustmentsController.php
@@ -221,13 +221,13 @@ class ProductInAdjustmentsController extends Controller
                 ->editColumn('batch_number', '{{$batch_number}}')
                 ->editColumn('default_sell_price', function ($row) {
                     $price= AddStockLine::where('variation_id',$row->variation_id)
-                        ->whereColumn('quantity',">",'quantity_sold')->first();
+                        ->latest()->first();
                     $price= $price? ($price->sell_price > 0 ? $price->sell_price : $row->default_sell_price):$row->default_sell_price;
                     return $this->productUtil->num_f($price);
                 })//, '{{@num_format($default_sell_price)}}')
                 ->editColumn('default_purchase_price', function ($row) {
                     $price= AddStockLine::where('variation_id',$row->variation_id)
-                        ->whereColumn('quantity',">",'quantity_sold')->first();
+                    ->latest()->first();
                     $price= $price? ($price->purchase_price > 0 ? $price->purchase_price : $row->default_purchase_price):$row->default_purchase_price;
 
                     return $this->productUtil->num_f($price);
