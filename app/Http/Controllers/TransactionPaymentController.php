a9dcbca7f7d3fc86822992c47e9b5986d4568834
doaabakhiet <doaabakhiet11@gmail.com>
2023-08-09 09:01:00 -0700

    fix page reload

--- app/Http/Controllers/TransactionPaymentController.php
+++ app/Http/Controllers/TransactionPaymentController.php
@@ -94,7 +94,7 @@ class TransactionPaymentController extends Controller
      */
     public function store(Request $request)
     {
-        // try {
+        try {
             $data = $request->except('_token');
 
 
@@ -177,13 +177,13 @@ class TransactionPaymentController extends Controller
                 'success' => true,
                 'msg' => __('lang.success')
             ];
-        // } catch (\Exception $e) {
-        //     Log::emergency('File: ' . $e->getFile() . 'Line: ' . $e->getLine() . 'Message: ' . $e->getMessage());
-        //     $output = [
-        //         'success' => false,
-        //         'msg' => __('lang.something_went_wrong')
-        //     ];
-        // }
+        } catch (\Exception $e) {
+            Log::emergency('File: ' . $e->getFile() . 'Line: ' . $e->getLine() . 'Message: ' . $e->getMessage());
+            $output = [
+                'success' => false,
+                'msg' => __('lang.something_went_wrong')
+            ];
+        }
 
         if (request()->ajax()) {
             return $output;
--- resources/views/transaction_payment/add_payment.blade.php
+++ resources/views/transaction_payment/add_payment.blade.php
@@ -97,7 +97,7 @@
 
         <div class="modal-footer">
             <button type="button" id="submit_form_button" class="btn btn-primary">@lang('lang.save')</button>
-            <button type="button"id="close_modal_button" class="btn btn-default" data-dismiss="modal">@lang('lang.close')</button>
+            <button type="button" id="close_modal_button" class="btn btn-default" data-dismiss="modal">@lang('lang.close')</button>
         </div>
 
         {!! Form::close() !!}
@@ -127,7 +127,7 @@
                     console.log(response);
                   
                     $('#add_payment_form')[0].reset();
-                    $('#close_modal_button').click();
+                    $('#payment_modal').click();
                     $('#sales_table').DataTable().ajax.reload();
                 },
                 error: function(error) {
