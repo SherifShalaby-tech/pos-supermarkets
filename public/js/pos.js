8cf0ac1496e00f46ddf5122dd6dea0274314eb3f
doaabakhiet <doaabakhiet11@gmail.com>
2023-08-12 03:40:08 -0700

    error on add payment more than once

--- public/js/pos.js
+++ public/js/pos.js
@@ -2447,7 +2447,7 @@ $(document).on("submit", "form#add_payment_form", function (e) {
                 swal("Error", result.msg, "error");
             }
             $(".view_modal").modal("hide");
-            // get_recent_transactions();
+            get_recent_transactions();
         },
     });
 });
