12375919df836f21848b9c599c58193356b176c2
Raneem-ayman <73843554+Raneem-ayman@users.noreply.github.com>
2023-09-17 12:23:16 +0300

    Update SellPosController.php

--- app/Http/Controllers/SellPosController.php
+++ app/Http/Controllers/SellPosController.php
@@ -1473,6 +1473,8 @@ class SellPosController extends Controller
                 ->editColumn('status', function ($row) {
                     if ($row->status == 'canceled') {
                         return '<span class="badge badge-danger">' . __('lang.cancel') . '</span>';
+                    } elseif ($row->status == 'final' && $row->payment_status == 'pending') {
+                        return '<span class="badge badge-warning">' . __('lang.pay_later') . '</span>';
                     } else {
                         return '<span class="badge badge-success">' . ucfirst($row->status) . '</span>';
                     }
