34f57350fc7f3d49bcf3dcc6bc14182636c282d3
nour-alshikh <nooralshikh30@gmail.com>
2023-10-05 14:05:18 +0300

    pay fix

--- resources/views/sale_pos/partials/product_row.blade.php
+++ resources/views/sale_pos/partials/product_row.blade.php
@@ -204,7 +204,7 @@
                         name="transaction_sell_line[{{ $loop->index + $index }}][product_discount_type]"
                         value="{{ $convertedDiscountType }}">
 
-
+                    transaction_sell_line[{{ $loop->index + $index }}][product_discount_value]
                     <input type="hidden"
                         class="form-control product_discount_value  discount_value{{ $product->product_id }}"
                         name="transaction_sell_line[{{ $loop->index + $index }}][product_discount_value]"
