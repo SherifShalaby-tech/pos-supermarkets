46a696049849833572daea6f2b4d10eec6794090
mahmoudtokhey <mahmoudtokhey@gmail.com>
2023-11-13 23:01:22 +0200

    Test

--- resources/views/reports/customer_report.blade.php
+++ resources/views/reports/customer_report.blade.php
@@ -161,7 +161,7 @@
                 },
                 ajax: {
                     url: "/report/get-customer-report",
-                    // method:"post",
+                    method:"post",
                     data: function(d) {
                         d.customer_id = $("#customer_id").val();
                         d.customer_type_id = $("#customer_type_id").val();
